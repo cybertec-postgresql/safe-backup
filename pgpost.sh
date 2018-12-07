8b0c200ac053c975a7c77d23b51442a6e9f5256e
Laurenz Albe <laurenz.albe@cybertec.at>
2018-12-07 19:40:17 +0100

    Add a 60 second timeout where the post-script waits for the pre-script

--- pgpost.sh
+++ pgpost.sh
@@ -20,14 +20,25 @@ if [ $? -ne 0 ]; then exit 1; fi
 echo "UPDATE backup SET state = 'done'::backup_state;" >&${PSQL[1]}
 if [ $? -ne 0 ]; then exit 1; fi
 
-#wait for pg_stop_backup() to finish
+# wait a minute for pg_stop_backup() to finish
+declare -i secs_waited=0
 while
 	echo 'SELECT state FROM backup;' >&${PSQL[1]}
 	if [ $? -ne 0 ]; then exit 1; fi
 	read -u ${PSQL[0]} line
 	[ "$line" = 'done' ]
 do
+	if [ "$secs_waited" -gt 60 ]; then
+		echo "UPDATE backup SET
+			state = 'failed'::backup_state;" >&${PSQL[1]}
+		echo '\q' >&${PSQL[1]}
+		echo "pre-backup script failed to complete backup" 1>&2
+		exit 1
+	fi
+
 	sleep 5
+
+	secs_waited=secs_waited+5
 done
 
 if [ "$line" != 'complete' ]; then
